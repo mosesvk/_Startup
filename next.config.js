c3d92b4f6b885b0af95cc79e250ae1e06ae8fdce
mosesvk <kaumatule93@gmail.com>
2022-04-01 19:50:27 -0600

    configurations

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
+// plugins that we'll be using in our project
+
 module.exports = withPlugins([optimizedImages], { target: 'serverless' });
