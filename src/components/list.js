be8f680f28370bc0505c88b70c524d9cde9551eb
mosesvk <kaumatule93@gmail.com>
2022-04-02 22:19:29 -0600

    workflow Section

--- src/components/list.js
+++ src/components/list.js
@@ -3,7 +3,25 @@ import { Flex, Box, IconButton } from 'theme-ui';
 
 export default function List({ items = [], parentStyle, childStyle }) {
   return (
-    <h1>List</h1>
+    <Box
+      as='ul'
+      sx={{ listStyleType: 'none', margin: 0, padding: 0, ...parentStyle }}
+    >
+      {/* icon, text, isAvailable */}
+      {items.map((item, idx) => (
+        <Flex
+          className={isAvailable ? 'open' : 'closed'}
+          as='li'
+          sx={{...childStyle}}
+          key={idx}
+        >
+          <IconButton sx={styles.listIcon} aria-label='list icon'>
+            {icon}
+          </IconButton>
+          {item.text}
+        </Flex>
+      ))}
+    </Box>
   );
 }
 
